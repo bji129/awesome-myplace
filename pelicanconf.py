ff9f5204327392d04aba0012bde4b2cc04d69abc
Brandon Ji <bji129@163.com>
2022-07-22 15:38:07 +0800

    test
    test

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,14 +24,16 @@ LINKS = (('博客工具', 'http://getpelican.com/'),
          ('在线编辑', 'https://mermaid.live/'),
          ('UML画图', 'https://draw.io/'),
          ('七外课程表', 'http://www.jideze.com/others/QFL-13.html'),
-         ('', '#'))
+         ('动态数据表', 'https://datatables.net/')
+         ('联系我', 'https://about.me/brandon.ji'),
+         ('个人资料', 'https://cn.linkedin.com/in/bji129'))
 
 # Social widget
-SOCIAL = (('代码库', 'http://github.com/bji129'),
-          ('个人建立', 'https://cn.linkedin.com/in/bji129'),
-          ('联系方式', 'https://about.me/bji129'))
+SOCIAL = (('代码库', 'http://github.com/bji129'))
 
 DEFAULT_PAGINATION = 4
+
+THEME = 'chunk'
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
