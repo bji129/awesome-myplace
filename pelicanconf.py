d7ae4c50b1023cde78643ac4a1a1b956ec391fe7
Brandon Ji <bji129@163.com>
2022-07-22 16:18:21 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ SOCIAL = (('代码库', 'http://github.com/bji129'),)
 
 DEFAULT_PAGINATION = 4
 
-THEME = 'simple'
+THEME = 'Chunk'
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
